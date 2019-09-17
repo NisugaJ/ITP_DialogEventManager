e300578058383c415aacbbbe1dcace187a6dfe24
Nisuga Jayawardana <44756803+NisugaJ@users.noreply.github.com>
2019-09-17 13:11:36 +0530

    update 9/17/2019

--- js/inserOrganizer.js
+++ js/inserOrganizer.js
@@ -1,23 +1,27 @@
 var databaseRef = firebase.database().ref('users/');
 
 function save_event() {
-
-    var oid = firebase.database().ref().child('users').push().key;
-    var sss = document.getElementById('id').value;
-    console.log(sss);
     var name = document.getElementById('Name').value;
     var position = document.getElementById('Position').value;
     var ratings = document.getElementById('Ratings').value;
-    var desExecu_level = document.getElementById('Execution_level').value;
     var Gmail = document.getElementById('Gmail').value;
     var password = document.getElementById('password').value;
+
+    //validation
+    if (name == "" || position == "" || ratings == "" || Gmail == "" || password == "") {
+        alert("Please Enter All data");
+        return;
+    }
+
+    var oid = firebase.database().ref().child('users').push().key;
+
+
 
     var data = {
         Organizer_ID: oid,
         Name: name,
         position: position,
         Ratings: ratings,
-        execution_level: desExecu_level,
         gmail: Gmail,
         password: password,
     }
@@ -37,7 +41,6 @@ function update_event() {
     var name = document.getElementById('Name').value;
     var position = document.getElementById('Position').value;
     var ratings = document.getElementById('Ratings').value;
-    var desExecu_level = document.getElementById('Execution_level').value;
     var Gmail = document.getElementById('Gmail').value;
     var password = document.getElementById('password').value;
 
@@ -46,15 +49,13 @@ function update_event() {
         Name: name,
         position: position,
         Ratings: ratings,
-        execution_level: desExecu_level,
         gmail: Gmail,
         password: password,
     }
     var updates = {};
 
-    if (org_id != "") {
-        updates['/users/' + org_id] = data;
-        firebase.database().ref().update(updates);
-    }
+    updates['/users/' + org_id] = data;
+    firebase.database().ref().update(updates);
+
     alert("organizer is updated successfully");
 }
