5f2c7dd93a9eea8c9eb64d9ce96758db483c6d0c
janitha_Akalanka <janithaakalanka45@gmail.com>
2019-09-17 17:03:58 +0530

    Organizer CRUD  done

--- js/organizerTable.js
+++ js/organizerTable.js
@@ -12,37 +12,47 @@ databaseRef.once('value', function(snapshot) {
             var childData = childSnapshot.val();
 
             var row = event.insertRow(rowIndex);
-            var cellId = row.insertCell(0);
-            var cellname = row.insertCell(1);
-            var cellposition = row.insertCell(2);
-            var cellratings = row.insertCell(3);
-            var cellgmail = row.insertCell(4);
-            var cellpassword = row.insertCell(5);
-            var editCell = row.insertCell(6);
-            var deleteCell = row.insertCell(7);
+            //  var cellId = row.insertCell(0);
+            var cellname = row.insertCell(0);
+            var cellposition = row.insertCell(1);
+            var cellratings = row.insertCell(2);
+            var cellgmail = row.insertCell(3);
+            var cellpassword = row.insertCell(4);
 
-            cellId.appendChild(document.createTextNode(childKey));
+            var editCell = row.insertCell(5);
+            var deleteCell = row.insertCell(6);
 
+            //cellId.appendChild(document.createTextNode(childKey));
             cellname.appendChild(document.createTextNode(childData.Name));
             cellposition.appendChild(document.createTextNode(childData.position));
             cellratings.appendChild(document.createTextNode(childData.Ratings));
             cellgmail.appendChild(document.createTextNode(childData.gmail));
             cellpassword.appendChild(document.createTextNode(childData.password));
 
-            editCell.innerHTML = '<img src="../images/edit.png" width="25" height="25>';
-            deleteCell.innerHTML = '<img src="../images/delete.png" width="25" height="25" >';
+            editCell.innerHTML = '<a href = "../html/UpdateOrganizerNew.html"><img src="../images/edit.png" width="25" height="25">';
+            deleteCell.innerHTML = '<img src="../images/delete.png" width="25" height="25">';
 
+            //call the Delete function 
             deleteCell.onclick = function() {
                 delete_event(childKey);
             }
 
+            //call the update function
+            editCell.onclick = function() {
+                setSessionForUpdate(childKey, childData.Name, childData.position, childData.Ratings, childData.gmail, childData.password);
+            }
+
             rowIndex = rowIndex + 1;
         }
+
     });
 });
 
+//Delete Function
+
 function delete_event(organizer_id) {
     //var organizer_id = document.getElementById('id').value;
+
 
     firebase.database().ref().child('/users/' + organizer_id).remove();
 
@@ -53,4 +63,16 @@ function delete_event(organizer_id) {
 
 function reload_page() {
     window.location.reload();
+}
+
+//update Function
+
+function setSessionForUpdate(Organizers_ID, Name, Position, MobileNumber, Email, LoginPWD) {
+    console.log("making ses")
+
+    let OrganizerNew = [Organizers_ID, Name, Position, MobileNumber, Email, LoginPWD];
+    let str = JSON.stringify(OrganizerNew);
+
+    sessionStorage.setItem("OrganizerNew", str);
+
 }
