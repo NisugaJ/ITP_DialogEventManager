afdaaf79d24b9f7a86959eb3c6a57ce810b52fdf
Nisuga Jayawardana <44756803+NisugaJ@users.noreply.github.com>
2019-09-18 19:23:52 +0530

    Refined UpdateEvent function

--- js/eventTable.js
+++ js/eventTable.js
@@ -2,72 +2,65 @@ var databaseRef = firebase.database().ref('events/');
 
 var rowIndex = 1;
 
-function check_blank() {
-    var topic = document.getElementById('topic');
-    var stime = document.getElementById('start_time');
-    var eTime = document.getElementById('end_time');
-    var date = document.getElementById('date');
-    var location = document.getElementById('location');
+databaseRef.once('value',function(snapshot){
 
-    if (topic == null || stime == null || eTime == null || date == null || location == null) {
-        alert("Please Enter All data");
-    } else {
+  var event = document.getElementById('event_table');
 
+  snapshot.forEach(function(childSnapshot){
+    var childKey = childSnapshot.key;
+    var childData = childSnapshot.val();
+
+    var row = event.insertRow(rowIndex);
+    var cellId = row.insertCell(0);
+    var cellTItle = row.insertCell(1);
+    var cellDate = row.insertCell(2);
+    var cellST = row.insertCell(3);
+    var cellET = row.insertCell(4);
+    var cellDescription = row.insertCell(5);
+    var cellLocation = row.insertCell(6);
+    var editCell = row.insertCell(7);
+    var deleteCell = row.insertCell(8);
+
+    cellId.appendChild(document.createTextNode(childKey));
+
+    cellTItle.appendChild(document.createTextNode(childData.event_topic));
+    cellDate.appendChild(document.createTextNode(childData.date));
+    cellST.appendChild(document.createTextNode(childData.start_time));
+    cellET.appendChild(document.createTextNode(childData.end_time));
+    cellDescription.appendChild(document.createTextNode(childData.description));
+    cellLocation.appendChild(document.createTextNode(childData.location));
+
+    editCell.innerHTML = '<a href = "../html/registerEvent.html"><img src="../images/edit.png" width="25" height="25"></a>';
+    deleteCell.innerHTML = '<img src="../images/delete.png" width="25" height="25">';
+
+    deleteCell.onclick = function() {
+      delete_event(childKey);
     }
-}
 
-databaseRef.once('value', function(snapshot) {
+    editCell.onclick = function() {
+      set_session(childKey,childData.event_topic,childData.date,childData.start_time,childData.end_time,childData.description,childData.location);
+    }
 
-    var event = document.getElementById('event_table');
-
-    snapshot.forEach(function(childSnapshot) {
-        var childKey = childSnapshot.key;
-        var childData = childSnapshot.val();
-
-        var row = event.insertRow(rowIndex);
-        var cellId = row.insertCell(0);
-        var cellTItle = row.insertCell(1);
-        var cellDate = row.insertCell(2);
-        var cellST = row.insertCell(3);
-        var cellET = row.insertCell(4);
-        var cellDescription = row.insertCell(5);
-        var cellLocation = row.insertCell(6);
-        var editCell = row.insertCell(7);
-        var deleteCell = row.insertCell(8);
-
-        cellId.appendChild(document.createTextNode(childKey));
-
-        cellTItle.appendChild(document.createTextNode(childData.event_topic));
-        cellDate.appendChild(document.createTextNode(childData.date));
-        cellST.appendChild(document.createTextNode(childData.start_time));
-        cellET.appendChild(document.createTextNode(childData.end_time));
-        cellDescription.appendChild(document.createTextNode(childData.description));
-        cellLocation.appendChild(document.createTextNode(childData.location));
-
-        editCell.innerHTML = '<a href="../html/registerEvent.html"><img src="../images/edit.png" width="25" height="25"></a>';
-        deleteCell.innerHTML = '<img src="../images/delete.png" width="25" height="25">';
-
-        deleteCell.onclick = function() {
-            delete_event(childKey);
-        }
-
-        editCell.onclick = function() {
-            check_blank_update(childKey, cellTItle, childData.date, childData.start_time, childData.end_time, childData.description, childData.location);
-        }
-
-        rowIndex = rowIndex + 1;
-    });
+    rowIndex = rowIndex + 1;
+  });
 });
 
-function delete_event(id) {
-    alert("Event " + id + " is deleting now");
+function delete_event (id){
 
-    firebase.database().ref().child('/events/' + id).remove();
+  firebase.database().ref().child('/events/' + id).remove();
 
-    reload_page();
+  alert("The user is deleted successfully");
+  reload_page();
 }
 
+function reload_page(){
+  window.location.reload();
+}
 
-function reload_page() {
-    window.location.reload();
+function set_session(id,title,date,stime,etime,desc,loca){
+
+  let event = [id,title,date,stime,etime,desc,loca];
+  let str = JSON.stringify(event);
+
+  sessionStorage.setItem("event",str);
 }
